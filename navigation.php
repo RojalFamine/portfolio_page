8f233914406f587cb86995dfabbd50627bc1edd7
RoyalFamine <rojal7famine@gmail.com>
2021-08-31 23:22:22 +0300

    aaand index file goes to html format

--- navigation.php
+++ navigation.php
@@ -1,7 +1,7 @@
 <nav class="main-nav">
     <!-- ***** Logo Start ***** -->
     <!-- <a href="home.php" class="logo">M<em> H</em></a> -->
-    <a href="index.php" class="logo"><img class="logo-b" style="height: 42px;" src="assets/images/martina.png" alt=""><img class="logo-w" style="height: 42px;" src="assets/images/martina-white.png" alt=""></a>
+    <a href="index.html" class="logo"><img class="logo-b" style="height: 42px;" src="assets/images/martina.png" alt=""><img class="logo-w" style="height: 42px;" src="assets/images/martina-white.png" alt=""></a>
     <!-- ***** Logo End ***** -->
     <!-- ***** Menu Start ***** -->
     <ul class="nav">
